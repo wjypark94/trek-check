b44469be781d1935c4b2ce9613a6c2449a5b273b
William Park <wjypark94@gmail.com>
2018-03-10 17:18:44 -0800

    able to display hiking trails with basic info

--- index.js
+++ index.js
@@ -1,57 +1,95 @@
-const HIKE_SEARCH_URL = 'https://api.foursquare.com/v2/venues/search';
-const CLIENT_ID = "ISY5MALGO3AVEOSEERX5ZUGGQWXAPSFPC2YJV004AHCDML10";
-const CLIENT_SECRET = "KTCJFLIVGG1GKB3M4P3WVY0Y5H5UEMJOWNRSH3VUDROOS1HE";
-const HIKE_PHOTO_URL = 'https://api.foursquare.com/v2/venues/';
+const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore?&client_id=FPRD2S2RFIB4QLBNBBHNAMLYOUF2AZSZ21ZK53QYASWCRJ1Z&client_secret=FEFA44EG0YDZ0XKA1UWX5ZWLZJLE30E2GYRLGB44PKE5KZ0E&v=20170915"
 
-function getDataFromFourApi(searchTerm, callback){
-    const settings = {
-        url: HIKE_SEARCH_URL + '?' + "v=20180308" + '&near=Los Angeles' + "&query=hiking" + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET,
-        dataType: 'json',
-        type: 'GET',
-        success: callback
-    };
-    $.ajax(settings);
-};
+//press on submit button and scroll to results
+function scrollPageTo(myTarget, topPadding) {
+    if (topPadding == undefined) {
+        topPadding = 0;
+    }
+    var moveTo = $(myTarget).offset().top - topPadding;
+    $('html, body').stop().animate({
+        scrollTop: moveTo
+    }, 200);
+}
 
-function displayResult(result){
+//retrieve data from FourSquare API
+function getDataFromFourApi() {
+        let city = $('.search-query').val();
+        let category = $(this).text();
+        $.ajax(FOURSQUARE_SEARCH_URL, {
+            data: {
+                near: city,
+                venuePhotos: 1,
+                limit: 20,
+                query: 'trail',
+            },
+            dataType: 'json',
+            type: 'GET',
+            success: function (data) {
+                try {
+                    let results = data.response.groups[0].items.map(function (item, index) {
+                        return displayResults(item);
+                    });
+                    $('#foursquare-results').html(results);
+                    scrollPageTo('#foursquare-results', 15);
+                } catch (e) {
+                    $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
+                }
+            },
+            error: function () {
+                $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
+            }
+        });
+        console.log(category);
+  
+}
+
+function displayResults(result) {
+
+    console.log(result.venue)
     return `
-      <ul> 
-        <li>
-        ${result.name}
-        </li>
-      </ul>
-    `;
+        <div class="result col-3">
+            <div class="result-image" style="background-image: url(https://igx.4sqi.net/img/general/width960${result.venue.photos.groups[0].items[0].suffix})" ;>
+            </div>
+            <div class="result-description">
+                <h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
+                <span class="icon">
+                    <img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
+                </span>
+                <span class="icon-text">
+                    ${result.venue.categories[0].name}
+                </span>
+                <p class="result-address">${result.venue.location.formattedAddress[0]}</p>
+                <p class="result-address">${result.venue.location.formattedAddress[1]}</p>
+                <p class="result-address">${result.venue.location.formattedAddress[2]}</p>
+            </div>
+        </div>
+`;
 }
 
-function displayPhotos(result, callback){
-    const settings = {
-        url: HIKE_PHOTO_URL + result.id + "/photos?" + "v=20180308" + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET,
-        dataType: 'json',
-        type: 'GET',
-        success: callback
-    };
-    $.ajax(settings);
-}
 
-function displayPhotoResults(data){
-    console.log(data);
-}
+function searchLocation() {
+    $('.category-button').click(function () {
+        $('button').removeClass("selected");
+        $(this).addClass("selected");
+    });
 
-function displayYelpSearchResults(data){
-    console.log(data);
-    const searchResults = data.response.venues.map((item, index) => displayResult(item));
-    const searchPhotos = data.response.venues.map((item, index) => displayPhotos(item, displayPhotoResults))
-    $('.js-search-results').html(searchResults);
-}
-
-function watchSubmit(){
-    $('.js-search-form').submit(function(event){
+    $('.search-form').submit(function (event) {
         event.preventDefault();
-        let queryTarget = $(event.currentTarget).find('.js-query');
-        let query = queryTarget.val();
-        queryTarget.val("");
-        getDataFromFourApi(query, displayYelpSearchResults);
+        $('.navigation').removeClass("hide");
+        $('#foursquare-results').html("");
+        getDataFromFourApi();
+        $('button').removeClass("selected");
     });
 }
 
-$(watchSubmit);
+//autocomplete location name in form
+function activatePlacesSearch() {
+    let options = {
+        types: ['(regions)']
+    };
+    let input = document.getElementById('search-term');
+    let autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+
+
+$(searchLocation);
